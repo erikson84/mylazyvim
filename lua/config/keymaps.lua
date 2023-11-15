1c8af81cbc09af0dd91bb04d28db73225c1eae7b
Erikson Kaszubowski <erikson23@gmail.com>
2023-11-14 22:25:59 -0300

    Configure keymaps to make terminal work on windows

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+
+local lazyterm = function()
+  Util.terminal(nil, { cwd = Util.root() })
+end
+vim.keymap.set("n", "<c-\\>", lazyterm, { desc = "Terminal (root dir)" })
+vim.keymap.set("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
