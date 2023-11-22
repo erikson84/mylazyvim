d74691a53ab4e2d54721d0a29845a81eedd8f082
Erikson Kaszubowski <erikson.k@ufsc.br>
2023-11-22 17:29:13 -0300

    NOW the configuration is indeed correct

--- lua/plugins/projects.lua
+++ lua/plugins/projects.lua
@@ -1,8 +1,11 @@
 return {
   "ahmedkhalf/project.nvim",
-  config = function()
-    require("project_nvim").setup({
-      patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "mix.exs" },
-    })
-  end,
+  --config = function()
+  -- require("project_nvim").setup({
+  opts = {
+    manual_mode = false,
+    patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "mix.exs" },
+  },
+
+  --end,
 }
