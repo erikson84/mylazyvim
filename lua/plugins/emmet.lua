18654da29fbd8e39488213a46428579bc2ec09a2
Erikson Kaszubowski <erikson23@gmail.com>
2024-01-03 00:31:42 -0300

    Configure emmet correctly

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -1,8 +1,22 @@
 return {
   {
-    "olrtg/emmet-language-server",
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
     opts = {
-      { filetypes = { "heex", "eex" } },
+      ---@type lspconfig.options
+      servers = {
+        emmet_language_server = {
+          filetypes = { "heex", "eex" },
+          init_options = {
+            html = {
+              options = {
+                -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+                ["bem.enabled"] = true,
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
