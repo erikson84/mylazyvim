fba3d6931f8544ffd3da78a21be0016f07e0f375
Erikson Kaszubowski <erikson23@gmail.com>
2024-07-16 00:35:03 -0300

    Better Telescope find_files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,10 +3,30 @@
 -- Add any additional keymaps here
 local Util = require("lazyvim.util")
 
+function vim.find_files_from_project_git_root()
+  local function is_git_repo()
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    return vim.v.shell_error == 0
+  end
+  local function get_git_root()
+    local dot_git_path = vim.fn.finddir(".git", ".;")
+    return vim.fn.fnamemodify(dot_git_path, ":h")
+  end
+  local opts = {}
+  if is_git_repo() then
+    opts = {
+      cwd = get_git_root(),
+    }
+  end
+  require("telescope.builtin").find_files(opts)
+end
+
 local lazyterm = function()
   Util.terminal(nil, { cwd = Util.root() })
 end
 vim.keymap.set("n", "<c-\\>", lazyterm, { desc = "Terminal (root dir)" })
 vim.keymap.set("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-vim.keymap.del("n", "<leader><space>")
-vim.keymap.set("n", "<leader><space>", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+if not vim.g.vscode then
+  vim.keymap.del("n", "<leader><space>")
+  vim.keymap.set("n", "<leader><space>", vim.find_files_from_project_git_root, { desc = "Find files" })
+end
