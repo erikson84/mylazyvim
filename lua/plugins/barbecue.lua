5f90a3da4f785205b43deefbd052494b6ca6e145
Erikson Kaszubowski <erikson23@gmail.com>
2024-07-18 00:53:35 -0300

    correct elixir icon in barbecue.

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -4,10 +4,8 @@ return {
   version = "*",
   dependencies = {
     "SmiteshP/nvim-navic",
-    "nvim-tree/nvim-web-devicons", -- optional dependency
   },
   opts = {
-    -- configurations go here
     theme = "catppuccin-frappe",
   },
 }
