88697cd8766403fc2eabe26cd1c5cac26ca0c0e6
Erikson Kaszubowski <erikson.kaszubowski@serpro.gov.br>
2024-07-19 13:21:00 -0300

    Remove noice jdtls exclusion; include pom.xml in projects.

--- lua/plugins/projects.lua
+++ lua/plugins/projects.lua
@@ -2,6 +2,6 @@ return {
   "ahmedkhalf/project.nvim",
   opts = {
     manual_mode = false,
-    patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "mix.exs" },
+    patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "mix.exs", "pom.xml" },
   },
 }
