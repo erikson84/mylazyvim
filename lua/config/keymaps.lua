2fea85ef58a3e6cf2de0b6e0818964247065b700
Erikson Kaszubowski <erikson.kaszubowski@serpro.gov.br>
2024-11-12 15:15:06 -0300

    Change terminal to Snacks.terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Snack = require("snacks")
 local Util = require("lazyvim.util")
 
-local lazyterm = function()
-  Util.terminal(nil, { cwd = Util.root() })
+local snackterm = function()
+  Snack.terminal(nil, { cwd = Util.root() })
 end
-vim.keymap.set("n", "<c-\\>", lazyterm, { desc = "Terminal (root dir)" })
+vim.keymap.set("n", "<c-\\>", snackterm, { desc = "Terminal (root dir)" })
 vim.keymap.set("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
