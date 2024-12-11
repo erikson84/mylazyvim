909868f53eaac6d0b141096f6143f0d36f16f125
Erikson Kaszubowski <erikson.kaszubowski@serpro.gov.br>
2024-12-11 18:28:19 -0300

    Modify snacks and context config.

--- lua/plugins/context.lua
+++ lua/plugins/context.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   opts = {
-    max_lines = 7
+    max_lines = 7,
+    multiline_threshold = 2,
+    trim_scope = 'outer',
+    mode = 'topline'
   }
 }
--- lua/plugins/snack-notifier.lua
+++ lua/plugins/snack-notifier.lua
@@ -2,5 +2,7 @@ return {
   "folke/snacks.nvim",
   opts = {
     notifier = { style = "fancy" },
+    scroll = { enabled = true },
+    words = { enabled = true }
   }
 }
