80418137495e0a6e69016966b02dbe68858a3d14
Erikson Kaszubowski <erikson.kaszubowski@serpro.gov.br>
2024-12-15 15:34:36 -0300

    Reconfigure blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,17 +4,6 @@ return {
     opts = {
       keymap = {
         preset = "super-tab",
-        ["<Tab>"] = {
-          function(cmp)
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          "snippet_forward",
-          "fallback",
-        },
       },
     },
   },
