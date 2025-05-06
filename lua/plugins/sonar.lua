c3d942bc6bcf80c55bd6d1afeb381f748714749c
Erikson Kaszubowski <erikson.kaszubowski@serpro.gov.br>
2025-05-06 10:14:11 -0300

    Config sonarqube

---
+++ lua/plugins/sonar.lua
@@ -0,0 +1,26 @@
+return {
+  url = "https://gitlab.com/schrieveslaach/sonarlint.nvim",
+  ft = { "python", "java", "javascript", "typescript" },
+  config = function()
+    require("sonarlint").setup({
+      server = {
+        cmd = {
+          "sonarlint-language-server",
+          -- Ensure that sonarlint-language-server uses stdio channel
+          "-stdio",
+          "-analyzers",
+          -- paths to the analyzers you need, using those for python and java in this example
+          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjs.jar"),
+        },
+      },
+      filetypes = {
+        "python",
+        "java",
+        "javascript",
+        "typescript",
+      },
+    })
+  end,
+}
